aff4dc19d01bded594fa1243d508c3419b3edcaf
CMParlett <chelseaparlett@gmail.com>
2016-06-28 12:12:25 -0700

    Span tag fix

--- Sternberg.js
+++ Sternberg.js
@@ -7,8 +7,9 @@ function circleofletters(number_of_letters) {
     return text;}
   for(var k=0, k<text.length, k++){
     var charstring = "char" + string(k+1)
-    var element = document.createElement("h1");
-    element.setAttribute("id", charstring);
-    document.getElementById(charstring).innerHTML = text[k]; //<h1 id="charstring"> text[k] </h1>
+    var element = document.getElementById("circle").createElement("span");
+    element.setAttribute("class", charstring);
+    document.getElementById("circle").innerHTML = text[k]; //<h1 id="charstring"> text[k] </h1>
   }
+
 }
