409fdf8f666ddaf1310c6b2c382c4d5276dd4747
CMParlett <chelseaparlett@gmail.com>
2016-06-28 10:54:02 -0700

    Sternberg js

--- Sternberg.js
+++ Sternberg.js
@@ -1,3 +1,14 @@
 function circleofletters(number_of_letters) {
   var degree_increments = 306/number_of_letters;
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = [];
+  for( var i=0; i < number_of_stim; i++ ){
+        text.push(possible.charAt(Math.floor(Math.random() * possible.length)));
+    return text;}
+  for(var k=0, k<text.length, k++){
+    var charstring = "char" + string(k+1)
+    var element = document.createElement("h1");
+    element.setAttribute("id", charstring);
+    document.getElementById(charstring).innerHTML = text[k];
+  }
 }
